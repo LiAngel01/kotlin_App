35fe7eadb5f47c6112e9a60d607b3926e2a1b245
LiAngel01 <133161998+LiAngel01@users.noreply.github.com>
2026-03-06 09:47:30 -0500

    Update MainActivity.kt

--- app/src/main/java/com/aodapp/MainActivity.kt
+++ app/src/main/java/com/aodapp/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.aodapp
 
 import android.content.Intent
-import android.os.Build
+import android.content.IntentFilter
 import android.os.Bundle
-import android.view.View
-import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var receiver: ScreenReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +17,13 @@ class MainActivity : AppCompatActivity() {
 
         val startButton = findViewById<Button>(R.id.startButton)
 
+        receiver = ScreenReceiver()
+
         startButton.setOnClickListener {
-            val intent = Intent(this, AODActivity::class.java)
-            startActivity(intent)
+
+            val filter = IntentFilter(Intent.ACTION_SCREEN_OFF)
+
+            registerReceiver(receiver, filter)
         }
     }
 }
