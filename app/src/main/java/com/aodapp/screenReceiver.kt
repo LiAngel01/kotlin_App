777270ae0aaac7b52a4ee22cf2355f0bb3602a3e
LiAngel01 <133161998+LiAngel01@users.noreply.github.com>
2026-03-06 09:22:49 -0500

    Create screenReceiver.kt

---
+++ app/src/main/java/com/aodapp/screenReceiver.kt
@@ -0,0 +1,20 @@
+package com.aodapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ScreenReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        if (intent.action == Intent.ACTION_SCREEN_OFF) {
+
+            val i = Intent(context, AODActivity::class.java)
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+            context.startActivity(i)
+        }
+
+    }
+}
