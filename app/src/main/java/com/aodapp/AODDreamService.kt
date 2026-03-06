adc145527ce26102ced26cb5a7314bb20085e322
LiAngel01 <133161998+LiAngel01@users.noreply.github.com>
2026-03-06 10:21:47 -0500

    Update AODDreamService.kt

--- app/src/main/java/com/aodapp/AODDreamService.kt
+++ app/src/main/java/com/aodapp/AODDreamService.kt
@@ -1,39 +1,44 @@
 package com.aodapp
 
 import android.service.dreams.DreamService
-import android.view.View
-import android.widget.TextView
 import android.os.Handler
 import android.os.Looper
+import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.*
 
 class AODDreamService : DreamService() {
 
     private lateinit var clockText: TextView
+
     private val handler = Handler(Looper.getMainLooper())
 
-    private val updateRunnable = object : Runnable {
-        override fun run() {
-            val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
-            clockText.text = timeFormat.format(Date())
-            handler.postDelayed(this, 60000)
-        }
-    }
-
     override fun onAttachedToWindow() {
+
         super.onAttachedToWindow()
 
-        isInteractive = false
-        isFullscreen = true
         setContentView(R.layout.activity_aod)
 
+        setInteractive(false)
+        setFullscreen(true)
+
         clockText = findViewById(R.id.clockText)
-        handler.post(updateRunnable)
+
+        updateClock()
     }
 
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        handler.removeCallbacks(updateRunnable)
+    private fun updateClock() {
+
+        handler.post(object : Runnable {
+
+            override fun run() {
+
+                val time = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
+
+                clockText.text = time
+
+                handler.postDelayed(this, 60000)
+            }
+        })
     }
 }
